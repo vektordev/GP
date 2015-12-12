e7e8b970ddef62fd3cbf2f63485dee027c16150f
vektordev <vpfanschilling@gmail.com>
2015-12-12 02:43:20 +0100

    discretized Histograms - or the raw data at least

--- src/GRPMath.hs
+++ src/GRPMath.hs
@@ -3,11 +3,13 @@ module GRPMath
 , mean
 , variance
 , rawHistogram
+, discretizeHistogram
 )
 where
 
 import Debug.Trace
 import Data.List
+import Data.Ord
 
 --Statistics functions
 --TODO: there's probably a gaussian package somewhere on hackage already.
@@ -23,6 +25,26 @@ variance lst =
 --TODO: diversity metric: ForAll pair of individuals, calculate distance to common ancestor, sum up.
 
 rawHistogram lst = map (\lst -> (length lst, head lst)) $ group $ sort lst
+
+--discretizeHistogram :: [(Int, t)] -> t -> [(Int, (t,t))]
+discretizeHistogram :: Float -> [(Int, Float)] -> [(Int, (Float, Float))]
+discretizeHistogram step hist =
+  let
+    intervalDelim = [snd $ head hist, (snd (head hist) + step).. (snd (last hist) + step)] --(snd (last hist) + step)]
+    intervals = [(0, (x,x+step)) | x <- intervalDelim]
+    zipping hist pt =
+      case hist of
+        [] -> either (\x -> [(0, (x, x))]) (error "discretizeHistogram: illegal state 1") pt
+        ((num, (begin, end)):xs) ->
+          case pt of
+            Left border -> (0, (border, border)):(num, (begin, border)):xs
+            Right (num2, val) -> (num2 + num, (begin, end)):xs
+  in
+    dropWhile (\x -> fst x == 0) $ reverse $ dropWhile (\x -> fst x == 0) $ foldl zipping [] (sortBy (comparing (either id snd)) (map Left intervalDelim ++ map Right hist))
+
+--do p <- loadFromFile "wedevetest-301"; putStrLn $ show $ rawHistogram $ map fitness $ flatten $ genomes p
+--do p <- loadFromFile "wedevetest-301"; putStrLn $ show $ rawHistogram  $ map compilationRate $ catMaybes $ iterateTZipper getFeatures $ fromTree $ genomes p
+--do p <- loadFromFile "/home/viktor/Dropbox/GA/data/wedtest-241"; putStrLn $ show $ discretizeHistogram 0.01 $ rawHistogram $ map (snd . getFitness) $ filter (\i -> (fst $ getFitness i) == Compilation) $ flatten $ genomes p
 
 --How to assign a number of tickets to a list of weights?
 weightedAssign :: (Integral a, Integral b, Functor t, Foldable t) => a -> t Float -> t b
