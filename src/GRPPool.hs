948687527dee7655f3bc5a69a9d036c9839b5ab5
vektordev <vpfanschilling@gmail.com>
2015-12-20 01:42:03 +0100

    oops

--- src/GRPPool.hs
+++ src/GRPPool.hs
@@ -384,7 +384,7 @@ createChild loc id srcCode = do
 filterPool :: Int -> Tree (Individual, Float) -> (Tree Individual, [String], [String], [String])
 filterPool min genomesAndFitness = --(fmap fst genomesAndFitness, [])
   --TODO: Does this filter need work?
-  if length (filter (\i -> case i of JunkI _ _ -> False; _ -> True) $ flatten pop) > min
+  if length (filter (\i -> case i of JunkI _ _ -> False; _ -> True) $ flatten pop) < min
   then (fmap fst reducedPop, genomeRemovals, [], [])
   else (fmap (\(a,_,_)-> a) result, genomeRemovals, recompilations, fileremovals)
   where
