c92531778cdef432f872f75c1ed3a72ae4727003
vektordev <vpfanschilling@gmail.com>
2015-12-23 22:08:55 +0100

    Bit of work on automatic detection of functions in scope within GRPSeed.hs

--- src/GRPDictionaryGenerator.hs
+++ src/GRPDictionaryGenerator.hs
@@ -8,6 +8,8 @@ import System.Random
 import Data.Char (isSpace)
 import Data.List
 import Data.List.Split
+import Language.Haskell.Exts.Parser
+import Language.Haskell.Exts.Extension
 
 data ProgramEnvironment = ProgramEnvironment [Instance] [Definition] deriving (Show, Read)
 
@@ -19,23 +21,36 @@ data TypeClass = TypeClass String deriving (Show, Read)
 
 --acquiring instance info can be done through use of :info Type
 
-mkDictionary :: String -> IO ()
-mkDictionary str = do--"echo ":browse Prelude" | ghci"
+mkDictionary :: IO ()
+mkDictionary = do--"echo ":browse Prelude" | ghci"
   (code, out, err) <- readProcessWithExitCode "ghci" ["GRPSeed.hs"] ":browse! *GRPSeed"
   print code
-  putStrLn out
   putStrLn "errors ahead"
   putStrLn err
-  putStrLn (show $ parseBrowseOutput out)
-  writeFile "Dictionary" (show $ parseBrowseOutput out)
+  --putStrLn (show $ parseBrowseOutput out)
+  parseResult <- parseBrowseOutput out
+  writeFile "Dictionary.hs" (show parseResult)
 
+parseBrowseOutput str = do
+  writeFile "rawDict.hs" ((unlines $ map show failedP) ++ (unlines $ map show succP))
+  putStrLn ("Failed: " ++ (show $ length failedP) ++ ", succeeded: " ++ (show $ length succP))
+  return $ parseDeclWithMode mode modStr
+  where
+    modStr = drop 10 $ unlines $ drop 4 $ take (length (lines str) - 1) $lines str -- remuve the CLI prompt and all the other prefix/suffix info
+    mode = defaultParseMode{baseLanguage = Haskell2010, extensions = extensions defaultParseMode ++ [EnableExtension ScopedTypeVariables]}
+    groupedLns :: [[String]]
+    groupedLns = drop 1 $ foldr (\line (grp:grps) -> if isSpace $ head line then (line:grp):grps else []:(line:grp):grps) [[]] $lines modStr
+    rawAndParseData = map (\lnGrp -> (concat lnGrp, parseModuleWithMode mode $ unlines lnGrp)) groupedLns
+    failedP = filter (\(grp, parse) -> case parse of ParseFailed _ _ -> True; otherwise -> False) rawAndParseData
+    succP = filter (\(grp, parse) -> case parse of ParseFailed _ _ -> False; otherwise -> True) rawAndParseData
+{-
 parseBrowseOutput :: String -> ProgramEnvironment
 parseBrowseOutput output =
   let
     filteredLns = drop 4 $ lines output
     preprocLns = init $ drop 10 (head filteredLns) : tail filteredLns
     groupedLns = foldr (\line (grp:grps) -> if isSpace $ head line then (line:grp):grps else []:(line:grp):grps) [[]] preprocLns
-  in (trace $ show groupedLns) ProgramEnvironment [] []
+  in (trace $ show groupedLns) ProgramEnvironment [] []-}
 
 parseFunction :: String -> Definition
 parseFunction str =
