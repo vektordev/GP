e6dd8572c7f36490f3e9f127498021c22477f2b5
vektordev <vpfanschilling@gmail.com>
2016-12-21 23:26:30 +0100

    fixed a bug where genome executables would look for non-existant files

--- src/Frontend.hs
+++ src/Frontend.hs
@@ -24,15 +24,18 @@ main = do
   pool <- loadFromFile (head args)
   display (InWindow ("Evaluation of " ++ head args) (1920, 1080) (0,0)) white (makePicture pool)
 
+idScaling = 140000
+
 makePicture :: Pool -> Picture
 makePicture p = Pictures [
     Translate (-150) (-110) $ summaryPrint p (catMaybes features),
-    Translate (-100) 0 $ plotFeatures "0 - fitness - 1" "0 - compilation rate - 1" fitness (fromRational . compilationRate) features, --(iterateTZipper getFeatures $ fromTree $ genomes p) (getWeights $ genomes p)
     Translate (-210) 0 $ plotFeatures "0 - children - 100" "0 - compilationRate - 1" (\fv -> ((1/100) * (fromIntegral $ GRPPool.children fv))) (fromRational . compilationRate) features,
+    Translate (-100) 0 $ plotFeatures "0 - fitness - 1" "0 - compilation rate - 1" fitness (fromRational . compilationRate) features, --(iterateTZipper getFeatures $ fromTree $ genomes p) (getWeights $ genomes p)
     Translate 10 0 $ plotFeatures "0 - children - 100" "-1 - cRateGain - 1" (\fv -> ((1/100) * (fromIntegral $ GRPPool.children fv))) (\fv -> (0.5) + 0.5 * fromRational (compilationRateGain fv)) features,
     Translate 120 0 $ plotFeatures "0 - compilationRate - 1" "-1 - cRateGain - 1" (fromRational . compilationRate) (\fv -> (0.5) + 0.5 * fromRational (compilationRateGain fv)) features,
-    Translate 230 0 $ plotFeatures "0 - id - 140000" "0 - compilationRate - 1" (\fv -> (fromIntegral $ GRPPool.id fv) / 140000) (fromRational . compilationRate) features,
-    Translate 340 0 $ plotFeatures "0 - id - 140000" "0 - fitness - 1" (\fv -> (fromIntegral $ GRPPool.id fv) / 140000) (fitness) features,
+    Translate 230 0 $ plotFeatures ("0 - id - "++ show idScaling) "0 - compilationRate - 1" (\fv -> (fromIntegral $ GRPPool.id fv) / idScaling) (fromRational . compilationRate) features,
+    Translate 340 0 $ plotFeatures ("0 - id - "++ show idScaling) "0 - fitness - 1" (\fv -> (fromIntegral $ GRPPool.id fv) / idScaling) (fitness) features,
+    Translate 450 0 $ plotFeatures ("0 - id - "++ show idScaling) "0 - children - 100" (\fv -> (fromIntegral $ GRPPool.id fv) / idScaling) (\fv -> ((1/100) * (fromIntegral $ GRPPool.children fv))) features,
     Translate (-210) 110 $ plotHistogram "compRate" "0 - number " (map (fromRational . compilationRate) $ catMaybes features) 0.001,
     Translate (-100) 110 $ plotHistogram "fitness" "0 - number " (map fitness $ catMaybes features) 0.01,
     Translate 10 110 $ plotHistogram "children" "0 - number " (map (fromIntegral . GRPPool.children) $ catMaybes features) 1,
@@ -65,10 +68,13 @@ summaryPrint p fvs = Scale 0.1 0.1 $ Pictures [
     Text ("Compiling genomes: "++ show (length $ filter (\ind -> getFitness ind >= (UnknownCompilerError, 10^10)) $ flatten $ genomes p))
   ]
 
+cross :: Float -> Picture
+cross axis = Pictures [Line [(-axis/2, -axis/2), (axis/2, axis/2)], Line [(axis/2, -axis/2), (-axis/2, axis/2)]]
+
 plotFeatures :: String -> String -> (FeatureVec-> Float) -> (FeatureVec-> Float) ->  [Maybe FeatureVec] -> Picture
 plotFeatures lb1 lb2 ft1 ft2 fs = Pictures (map plotFeaturesSingle fs ++ axes ++ labels)
   where
-    plotFeaturesSingle (Just fv) = Color (if GRPPool.id fv == 0 then green else if isLocalMax fv == LocalMax then red else if isLocalMax fv == Inherited then blue else  black) $ Translate (100 * (min 1 $ max 0 $ ft1 fv)) (100 * (min 1 $ max 0 $ ft2 fv)) $Circle  (0.0 + activeRegression fv)
+    plotFeaturesSingle (Just fv) = Color (if GRPPool.id fv == 0 then green else if isLocalMax fv == LocalMax then red else if isLocalMax fv == Inherited then blue else  black) $ Translate (100 * (min 1 $ max 0 $ ft1 fv)) (100 * (min 1 $ max 0 $ ft2 fv)) $ (if GRPPool.children fv > 50 then Circle else cross)  (0.0 + activeRegression fv)
     plotFeaturesSingle _ = Blank
     axes = [Line [(0,0),(0,100), (100,100),(100,0),(0,0)]] ++ if "cRateGain" `isInfixOf` lb2 then [Color (makeColor 0.5 0.5 0.5 0.5) $ Line [(0,50),(100,50)]] else []
     labels = map (Scale 0.05 0.05) [Translate 0 (-110) $ Text lb1 , Rotate (-90) $ Translate 0 10 $ Text lb2]
